4be6d54202af127f59118b0763d087f921baae42
Chamath Weerathunga <cnimesh11c@gmail.com>
2026-05-05 08:26:30 +0530

    unsupported font fix

---
+++ frontend/src/theme/fonts.js
@@ -0,0 +1,12 @@
+const fonts = {
+  regular: {
+    fontFamily: 'System',
+    fontWeight: '400',
+  },
+  medium: {
+    fontFamily: 'System',
+    fontWeight: '500',
+  },
+};
+
+export default fonts;
